ec540f886d879febab19dd70af9ca67b1011f7ae
Jordan Ruhle <jordanruhle@gmail.com>
2022-11-28 08:51:44 -0800

    Singly linked list

---
+++ java/wk1d1.js
@@ -0,0 +1,17 @@
+class SLL {
+    constructor() {
+        this.head = node
+    }
+}
+
+printValues() {
+    if(this.head === null){
+        console.log("SLL is Empty")
+        return
+    }
+    let runner = this.head
+    while(runner != null){
+        console.log(runner.value)
+        runner = runner.next
+    }
+}
