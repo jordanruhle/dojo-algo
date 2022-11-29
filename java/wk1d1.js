88eff741170facc2ae84f0614d6b35eabb7e2ca5
Jordan Ruhle <jordanruhle@gmail.com>
2022-11-29 08:18:01 -0800

    day 1

--- java/wk1d1.js
+++ java/wk1d1.js
@@ -2,16 +2,15 @@ class SLL {
     constructor() {
         this.head = node
     }
-}
-
-printValues() {
-    if(this.head === null){
-        console.log("SLL is Empty")
-        return
-    }
-    let runner = this.head
-    while(runner != null){
-        console.log(runner.value)
-        runner = runner.next
+    printValues() {
+        if(this.head === null){
+            console.log("SLL is Empty")
+            return
+        }
+        let runner = this.head
+        while(runner != null){
+            console.log(runner.value)
+            runner = runner.next
+        }
     }
 }
